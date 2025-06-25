49699b0bd539e359c4314a21f2683a7d731b3b14
Dillon Kearns <dillon@incrementalelm.com>
2025-06-25 14:58:50 -0700

    Add Facebook icon to footer.

--- app/Footer.elm
+++ app/Footer.elm
@@ -102,6 +102,29 @@ innerThing =
                         []
                     ]
                 ]
+            , Html.a
+                [ Attr.href "https://www.facebook.com/soirnoirjazz"
+                , Attr.target "_blank"
+                , Attr.class "text-gold-primary hover:text-gold-light focus:outline-none focus:ring-2 focus:ring-gold-primary focus:ring-offset-2 focus:ring-offset-noir-primary rounded-md"
+                ]
+                [ Html.span
+                    [ Attr.class "sr-only"
+                    ]
+                    [ Html.text "Facebook" ]
+                , Svg.svg
+                    [ SvgAttr.class "size-6"
+                    , SvgAttr.fill "currentColor"
+                    , SvgAttr.viewBox "0 0 24 24"
+                    , Attr.attribute "aria-hidden" "true"
+                    ]
+                    [ Svg.path
+                        [ SvgAttr.fillRule "evenodd"
+                        , SvgAttr.d "M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                        , SvgAttr.clipRule "evenodd"
+                        ]
+                        []
+                    ]
+                ]
             ]
         , Html.p
             [ Attr.class "mt-8 text-sm/6 text-text-tertiary md:order-1 md:mt-0"
